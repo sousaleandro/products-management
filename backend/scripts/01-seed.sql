fcd9b6fad2febc7b27d799820b9cc6d51bf8569a
Leandro Sousa <lgleandro14@gmail.com>
2024-01-26 15:33:57 -0300

    fix: scripts sql

--- backend/scripts/01-seed.sql
+++
@@ -1,8 +0,0 @@
-USE NunesSports;
-
-DELETE FROM products;
-
-INSERT INTO products (name, code, description, price) VALUES
-    ('Product 1', 'P001', 'Description 1', 19.99),
-    ('Product 2', 'P002', 'Description 2', 29.99),
-    ('Product 3', 'P003', 'Description 3', 39.99);
